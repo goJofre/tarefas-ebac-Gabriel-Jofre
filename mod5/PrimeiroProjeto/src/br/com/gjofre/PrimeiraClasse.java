786a9b9d836cd50526a09edfae079037f7243a9a
goJofre <gabrielgoj3@gmail.com>
2022-10-15 16:17:31 -0300

    Primeiro Projeto

---
+++ mod5/PrimeiroProjeto/src/br/com/gjofre/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+package br.com.gjofre;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Gabriel");
+	}
+
+}
---
+++ mod5/PrimeiroProjeto/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author gabriel
+ *
+ */
+module PrimeiroProjeto {
+}
